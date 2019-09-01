7661da891c9b7065be29a9230dec8fd317015d39
omar ibi <omaribi@MacBook-Air.local>
2019-09-01 00:02:22 -0400

    updated

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
+  .connect(String(process.env.MONGODB_URL), {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
